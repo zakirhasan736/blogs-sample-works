23193d38eaceae81b019a5ffcfe03d8ec2e756d5
zakir <zakirhossaib736@gmail.com>
2023-12-26 21:08:27 +0600

    updated

--- src/app/case-study/[category]/page.tsx
+++ src/app/case-study/[category]/page.tsx
@@ -3,7 +3,7 @@ import CaseStudyCategory from "@/pages/CaseStudy/CaseStudyDetails/CaseStudyCateg
 async function getData(category: any) {
 	try {
 		const res = await fetch(
-			`http://localhost:3000/data/case-study/${category}.json`,
+			`https://blogs-sample-works.vercel.app/data/case-study/${category}.json`,
 			{
 				cache: "no-store",
 			},
--- src/app/services/[category]/page.tsx
+++ src/app/services/[category]/page.tsx
@@ -4,7 +4,7 @@ import ServicesCategoriesPage from "@/pages/Services/Services-details/ServicesCa
 async function getData(category: any) {
 	try {
 		const res = await fetch(
-			`http://localhost:3000/data/services/${category}.json`,
+			`https://blogs-sample-works.vercel.app/data/services/${category}.json`,
 			{
 				cache: "no-store",
 			},
