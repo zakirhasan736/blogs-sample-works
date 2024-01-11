a951638b5bb9e7c23f485657f564ec1cfd115188
rayhanwd <rayhanbd4400@gmail.com>
2024-01-11 17:00:28 +0600

    r

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm use 20.5.0  # Adjust to the version you needok
 
 echo "Deployment started..."
 
