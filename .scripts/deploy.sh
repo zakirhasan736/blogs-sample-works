0b0099259008c6fc699eef991707142c6a10a7c3
rayhanwd <rayhanbd4400@gmail.com>
2024-01-11 17:30:02 +0600

    add ym:

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -e
 
+
 echo "Deployment started..."
 
 # Pull the latest version of the app
-git pull origin master
+git pull origin main
 echo "New changes copied to server !"
 
 echo "Installing Dependencies..."
-/root/.nvm/versions/node/v20.9.0/bin/npm install --yes
+npm install --yes
 
 echo "Creating Production Build..."
-/root/.nvm/versions/node/v20.9.0/bin/npm run build
+npm run build
 
 echo "PM2 Reload"
 /root/.nvm/versions/node/v20.9.0/bin/pm2 reload 0
+
+echo "PM2 Setup done!"
 
 echo "Deployment Finished!"
