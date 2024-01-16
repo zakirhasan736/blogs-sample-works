78955af23908dd3a9b90539d3b1f4b715b4247c0
zakir <zakirhossaib736@gmail.com>
2024-01-16 20:38:05 +0600

    updated

--- src/components/common/cliend-slider/clientSlider.tsx
+++ src/components/common/cliend-slider/clientSlider.tsx
@@ -1,9 +1,8 @@
-"use client";
 import React from 'react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Image from 'next/image';
+import {Image} from '@packages/packages';
 const ClientSlider = () => {
       const settings = {
 				speed: 3000,
