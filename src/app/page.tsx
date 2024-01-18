c42c30d75e7cc79e3d9a59de4fbd8858ac871cf6
zakir <zakirhossaib736@gmail.com>
2024-01-18 18:29:29 +0600

    new sersion testimonials added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,36 @@
 "use client";
-import AboutSection from "@/components/about-section/about-section";
 import ArticlesSection from "@/components/article-section/articles-section";
 import CaseStudySlider from "@/components/case-study-slider/case-study-slider";
-import ClientSection from "@/components/client-section/ClientSection";
 import HomeBanner from "@/components/common/banner/home-banner-2";
 import AboutUsSection from "@/components/component-v2/aboutus-section/aboutus";
 import ServicesSection from "@/components/component-v2/services-section/services";
 import GuidedSection from "@/components/guided-section/guided-section";
 import ParticularSecModal from "@/components/particular-sec-modal/particular-sec-modal";
-import TestimonialsSection from "@/components/testimonials-section/testimonials-section";
-import clientsData from "@data/home-page-data/clients.json";
+import TestimonialsVersionTwo from "@/components/testimonials-section/testimonials-v2";
 import SliderItemsData from "@data/home-page-data/home-case-study-slider.json";
 
 const Home = () => {
 	const testimonialsSliderData = [
 		{
-			title: "Heat Centre",
-			description:
-				"We collaborated with Particular for the redesign of our website. The team at Particular was fantastic, efficiently handling last-minute demands and employing a comprehensive design process. I strongly endorse Particular for anyone in need of website services.",
+			testisliderModalImag: "/images/testimonials-slides-modal-img-1.png",
+			testimonialsAutherBio: "Founder (Tsagiris Clinic)",
+			testimonialsAutherName: "- Dr. John Tsagiris",
+			testimonialsText:
+				"My partnership with Particular Agency for our web application development has been nothing short of remarkable.",
 		},
 		{
-			title: "Anantara",
-			description:
-				"Since our collaboration began, we've experienced consistent improvement in our marketing outcomes, social media engagement, and brand visibility. I highly recommend Particular to any expanding business in search of a dependable marketing ally.",
+			testisliderModalImag: "/images/testimonials-v2-img-2.jpg",
+			testimonialsAutherBio: "knights Rushmoore Academy, Marketing Manager",
+			testimonialsAutherName: "- Marcia Johnson",
+			testimonialsText:
+				"We collaborated with Particular for the redesign of our website. The team at Particular was fantastic.",
 		},
 		{
-			title: "Knight's Academy",
-			description:
-				"Our partnership with Particular Agency for our web application development has been nothing short of remarkable. Their team's expertise in crafting intuitive and engaging web apps has significantly elevated our online presence. The team's attention to detail, innovative approach, and responsiveness to our needs have been exceptional.",
-		},
-		{
-			title: "Heat Centre",
-			description:
-				"We collaborated with Particular for the redesign of our website. The team at Particular was fantastic, efficiently handling last-minute demands and employing a comprehensive design process. I strongly endorse Particular for anyone in need of website services.",
-		},
-		{
-			title: "Anantara",
-			description:
-				"Since our collaboration began, we've experienced consistent improvement in our marketing outcomes, social media engagement, and brand visibility. I highly recommend Particular to any expanding business in search of a dependable marketing ally.",
+			testisliderModalImag: "/images/testimonials-v2-img-3.jpg",
+			testimonialsAutherBio: "Leaders Rising, Founder",
+			testimonialsAutherName: "- Susie Rushden",
+			testimonialsText:
+				"Since our collaboration began, we've experienced consistent improvement in our marketing outcomes, social media engagement, and brand visibility.",
 		},
 	];
 	return (
@@ -46,10 +39,12 @@ const Home = () => {
 			<HomeBanner />
 			{/* home banner section end*/}
 			<AboutUsSection />
+			{/* home testimonials section */}
+			<TestimonialsVersionTwo TestiSlides={testimonialsSliderData} />
+			{/* home testimonials section end*/}
 			{/* home services section  */}
 			<ServicesSection />
 			{/* home services section  end*/}
-
 			{/* home guided section */}
 			<GuidedSection />
 			{/* home guided section end*/}
@@ -65,9 +60,6 @@ const Home = () => {
 			<ArticlesSection />
 			{/* home article section end*/}
 
-			{/* home testimonials section */}
-			<TestimonialsSection slides={testimonialsSliderData} />
-			{/* home testimonials section end*/}
 		</div>
 	);
 };
---
+++ src/components/testimonials-section/testimonials-v2.tsx
@@ -0,0 +1,83 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, EffectFade, Navigation  } from "swiper/modules";
+
+interface Slide {
+	testisliderModalImag: string;
+	testimonialsAutherBio: string;
+	testimonialsAutherName: string;
+	testimonialsText: string;
+}
+
+interface CaseStudySliderProps {
+	TestiSlides: Slide[];
+}
+
+const TestimonialsVersionTwo: React.FC<CaseStudySliderProps> = ({
+	TestiSlides,
+}) => {
+	return (
+		<section className="testimonials-section-v2">
+			<Swiper
+				spaceBetween={80}
+				effect={"fade"}
+				navigation={true}
+				loop={true}
+				autoplay={{
+					delay: 3000,
+					disableOnInteraction: false,
+				}}
+				modules={[EffectFade, Navigation, Autoplay]}
+				className="testimonials-slider-swiper max-w-[1440px] mx-auto">
+				{TestiSlides.map((slide, index) => (
+					<SwiperSlide key={index}>
+						<div className="Testimonials-left-cont testimonials-slide-items z-50 relative pb-[60px] h-[736px] pt-[76px] sm:pt-[55px]">
+							<div className="testimonials-slides-modal-img">
+								<Image
+									src={slide.testisliderModalImag}
+									alt="testimonials modal image"
+									width={1400}
+									height={736}
+									className="w-full absolute h-full object-cover rounded-[10px] top-0 max-w-[1440px] mx-auto right-0 -z-10 left-0"
+								/>
+							</div>
+							<div className="custom-container  max-w-[1160px]">
+								<div className="comon-title max-w-[547px] mb-20 sm:mb-10">
+									<h2 className="mb-[6px]">Our Testimonials</h2>
+									<span></span>
+								</div>
+								<div className="Testimonials-title-desc">
+									<p className="testimonials-desc-text sm:pl-4">
+										<span className="before-comma sm:pl-4">“</span>
+										{slide.testimonialsText}
+										<span className="after-comma">”</span>
+									</p>
+									<div className="auther-info-box mt-[13px]  sm:pl-4">
+										<h3 className="text-[20px] text-left text-neu-white font-normal font-tertery leading-[1.1]">
+											{slide.testimonialsAutherName}
+										</h3>
+										<h4 className="text-[18px] text-left text-neu-white font-tertery font-normal leading-normal mt-[5px]">
+											{slide.testimonialsAutherBio}
+										</h4>
+									</div>
+								</div>
+							</div>
+						</div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</section>
+	);
+};
+
+export default TestimonialsVersionTwo;
