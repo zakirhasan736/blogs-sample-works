8b8077f4245466bd0913e1a1855d9b1bbd4d14e9
zakir <zakirhossaib736@gmail.com>
2024-01-30 21:51:51 +0600

    updated hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import "@/styles/css/main.css";
 import { Urbanist, Inter } from "next/font/google";
 import React from "react";
+import Script from "next/script";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -32,58 +33,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className={`${inter.variable} ${urbanist.variable}`}>
-			<Head>
-				{/* Google Tag Manager */}
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAGS}',{
-	page_path: window.location.pathname,
-});
-            `,
-					}}
-				/>
-
-				{/* Google Analytics */}
-				<script
-					async
-					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-				/>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
-	page_path: window.location.pathname,
-  });
-            `,
-					}}
-				/>
-
-				{/* Hotjar */}
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-                (function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:3837364,hjsv:6};
-                    a=o.getElementsByTagName('head')[0];
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-					}}
-				/>
-			</Head>
-
 			<body className="pca-marketing-page-body md:border-0">
 				<noscript>
 					<iframe
@@ -95,6 +44,22 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 				<main>{children}</main>
 				<GoogleTagManager gtmId="GTM-KSLS5JR5" />
 				<GoogleAnalytics gaId="G-5VRY1RRCR1" />
+				{/* Hotjar */}
+				<Script
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+                (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3837364,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+					}}
+				/>
 			</body>
 		</html>
 	);
