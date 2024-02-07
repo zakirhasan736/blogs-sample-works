2435dfda6c19462180b5583c778a826fa1b8ee70
zakir <zakirhossaib736@gmail.com>
2024-02-07 22:37:43 +0600

    updated partnershipe section

--- src/components/component-v2/partner-section/partner.tsx
+++ src/components/component-v2/partner-section/partner.tsx
@@ -5,34 +5,17 @@ const PartnerSection = () => {
   return (
 		<section className="partner-section pb-[150px] sm:pb-[120px]">
 			<div className="custom-container max-w-[1220px]">
-				<div className="comon-title mb-11">
-					<h2>In partnership with</h2>
+				<div className="comon-title mb-[58px] max-w-[335px] sm:max-w-[247px] mx-auto">
+					<h2 className="">In partnership with</h2>
 					<span></span>
 				</div>
 				<div className="partner-wrapper">
-					<h3 className="partner-sec-main-title text-center flex items-center justify-center">
-						<Image
-							src="/images/partnar-brand-logo.svg"
-							width={180}
-							height={42}
-                            className='sm:w-[160px]'
-							alt="modal title image"
-						/>
-					</h3>
-					<div className="shape-indicator mx-auto max-w-[703px] lg:px-15 md:px-20  sm:px-[40px] block">
-						<Image
-							src="/images/shape-indicator-dvitder.svg"
-							width={703}
-							height={120}
-							alt="shape indicator image"
-						/>
-					</div>
-					<div className="partner-list-box flex items-start justify-between max-w-[898px] mx-auto pt-2">
+					<div className="partner-list-box flex items-start justify-between max-w-[683px] md:max-w-[483px] relative left-10 md:left-0 mx-auto">
 						<div className="partnar-list-items">
 							<Image
 								src="/images/DAN-member-badge-light-h 1.svg"
-								width={139}
-								height={68}
+								width={142}
+								height={126}
 								className="sm:w-[120px]"
 								alt="partner modal image"
 							/>
