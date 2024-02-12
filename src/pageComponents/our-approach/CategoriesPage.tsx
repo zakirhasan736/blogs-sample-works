0137da66283392b51316da5c791371756201cfef
zakir <zakirhossaib736@gmail.com>
2024-02-12 21:27:53 +0600

    updated statics data

--- src/pageComponents/our-approach/CategoriesPage.tsx
+++ src/pageComponents/our-approach/CategoriesPage.tsx
@@ -42,12 +42,12 @@ const CategoriesPage: React.FC<CategoryPageProps> = ({ data }) => {
 			description: "Completed more than 50 Successful Marketing Projects",
 		},
 		{
-			title: "£1-15k",
+			title: "1-60k",
 			description:
 				"Our current infrastructure allows us to easily cater for projects in this budgetrange",
 		},
 		{
-			title: "35%",
+			title: "47%",
 			description:
 				"Over 1 in 3 clients come back to us within  the first 3 months for additional marketing and branding support.",
 		},
