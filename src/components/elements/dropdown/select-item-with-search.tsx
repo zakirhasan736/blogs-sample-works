624270e70b08ddb98badcbaf91ac7af22fd61187
apixcel <apixcelv2@gmail.com>
2024-03-14 01:34:31 +0600

    add validation

--- src/components/elements/dropdown/select-item-with-search.tsx
+++ src/components/elements/dropdown/select-item-with-search.tsx
@@ -2,112 +2,119 @@ import Image from "next/image";
 import React, { useState, useEffect, useRef } from "react";
 
 interface SelectItemWithSearchProps {
-	items: string[];
-	onSelectedItemsChange: (selectedItems: string) => void;
-	placeholder: string;
-	SearchPlaceholder: string;
-	labelText?: string;
-	labeltextItem?: boolean;
+  items: string[];
+  onSelectedItemsChange: (selectedItems: string) => void;
+  placeholder: string;
+  SearchPlaceholder: string;
+  error: string;
+  value: any;
+  labelText?: string;
+  labeltextItem?: boolean;
 }
 
 const SelectItemWithSearch: React.FC<SelectItemWithSearchProps> = ({
-	items,
-	onSelectedItemsChange,
-	placeholder,
-	SearchPlaceholder,
-	labeltextItem,
-	labelText,
+  items,
+  onSelectedItemsChange,
+  placeholder,
+  SearchPlaceholder,
+  labeltextItem,
+  labelText,
+  error,
+  value
 }) => {
-	const [isOpen, setIsOpen] = useState(false);
-	const [searchTerm, setSearchTerm] = useState("");
-	const [selectedItems, setSelectedItems] = useState<string[]>([]);
-	const dropdownRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-	const toggleDropdown = () => {
-		setIsOpen(!isOpen);
-	};
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-	const handleItemClick = (item: string) => {
-		const updatedSelectedItems = selectedItems.includes(item)
-			? selectedItems.filter(i => i !== item)
-			: [...selectedItems, item];
+  const handleItemClick = (item: string) => {
+    const updatedSelectedItems = selectedItems.includes(item)
+      ? selectedItems.filter((i) => i !== item)
+      : [...selectedItems, item];
 
-		setSelectedItems(updatedSelectedItems);
-		onSelectedItemsChange(updatedSelectedItems.join(", "));
-	};
+    setSelectedItems(updatedSelectedItems);
+    onSelectedItemsChange(updatedSelectedItems.join(", "));
+  };
 
-	const handleOutsideClick = (event: MouseEvent) => {
-		if (
-			dropdownRef.current &&
-			!dropdownRef.current.contains(event.target as Node)
-		) {
-			setIsOpen(false);
-		}
-	};
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
 
-	useEffect(() => {
-		document.addEventListener("mousedown", handleOutsideClick);
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
 
-		return () => {
-			document.removeEventListener("mousedown", handleOutsideClick);
-		};
-	}, []);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
 
-	const filteredItems = items.filter(item =>
-		item.toLowerCase().includes(searchTerm.toLowerCase()),
-	);
+  const filteredItems = items.filter((item) =>
+    item.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-	return (
-		<div className="select-item-with-search relative" ref={dropdownRef}>
-			{labeltextItem && labelText && (
-				<label className="label-text text-[16px] text-left text-neu-white font-tertery font-bold leading-normal mb-[5px] block">
-					{labelText}
-				</label>
-			)}
-			<div className="input-item-select-filed-box relative">
-				<input
-					className="input-selected-data"
-					type="text"
-					value={selectedItems.join(", ")}
-					onClick={toggleDropdown}
-					placeholder={placeholder}
-					readOnly
-				/>
-				<span className="w-[6px] h-[19px] absolute top-0 bottom-0 right-3 mx-auto">
-					<Image
-						src="/images/icon.svg"
-						alt="arrow icons"
-						width={6}
-						height={19}
-					/>
-				</span>
-			</div>
-			{isOpen && (
-				<div className="dropdown-box">
-					<div className="search-bar px-[14px] py-[15px]">
-						<input
-							type="text"
-							value={searchTerm}
-							onChange={e => setSearchTerm(e.target.value)}
-							placeholder={SearchPlaceholder}
-						/>
-					</div>
-					<ul>
-						{filteredItems.map(item => (
-							<li key={item}>
-								<input
-									type="checkbox"
-									checked={selectedItems.includes(item)}
-									onChange={() => handleItemClick(item)}
-								/>
-								{item}
-							</li>
-						))}
-					</ul>
-				</div>
-			)}
-		</div>
-	);
+
+  return (
+    <div className="select-item-with-search relative" ref={dropdownRef}>
+      {labeltextItem && labelText && (
+        <label className="label-text text-[16px] text-left text-neu-white font-tertery font-bold leading-normal mb-[5px] block">
+          {labelText}
+        </label>
+      )}
+      <div className="input-item-select-filed-box relative">
+        <input
+          className="input-selected-data"
+          type="text"
+          value={value}
+          onClick={toggleDropdown}
+          placeholder={placeholder}
+          readOnly
+        />
+        <span className="w-[6px] h-[19px] absolute top-0 bottom-0 right-3 mx-auto">
+          <Image
+            src="/images/icon.svg"
+            alt="arrow icons"
+            width={6}
+            height={19}
+          />
+        </span>
+        {error ?? <p className="text-red-700">{error}</p>}
+      </div>
+      {isOpen && (
+        <div className="dropdown-box">
+          <div className="search-bar px-[14px] py-[15px]">
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder={SearchPlaceholder}
+            />
+          </div>
+          <ul>
+            {filteredItems.map((item) => (
+              <li key={item}>
+                <input
+                  type="checkbox"
+				  id={`checkbox-${item}`}
+                  checked={selectedItems.includes(item)}
+                  onChange={() => handleItemClick(item)}
+                />
+                <label htmlFor={`checkbox-${item}`} className="select-none">{item}</label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default SelectItemWithSearch;
