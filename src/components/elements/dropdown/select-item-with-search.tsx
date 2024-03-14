4fb1f3752d6fa5a8c53182c5acce6e734d4c2336
zakir <zakirhossaib736@gmail.com>
2024-03-14 10:32:15 +0600

    updated contact form requered

--- src/components/elements/dropdown/select-item-with-search.tsx
+++ src/components/elements/dropdown/select-item-with-search.tsx
@@ -63,58 +63,60 @@ const SelectItemWithSearch: React.FC<SelectItemWithSearchProps> = ({
 
 
   return (
-    <div className="select-item-with-search relative" ref={dropdownRef}>
-      {labeltextItem && labelText && (
-        <label className="label-text text-[16px] text-left text-neu-white font-tertery font-bold leading-normal mb-[5px] block">
-          {labelText}
-        </label>
-      )}
-      <div className="input-item-select-filed-box relative">
-        <input
-          className="input-selected-data"
-          type="text"
-          value={value}
-          onClick={toggleDropdown}
-          placeholder={placeholder}
-          readOnly
-        />
-        <span className="w-[6px] h-[19px] absolute top-0 bottom-0 right-3 mx-auto">
-          <Image
-            src="/images/icon.svg"
-            alt="arrow icons"
-            width={6}
-            height={19}
-          />
-        </span>
-        {error ?? <p className="text-red-700">{error}</p>}
-      </div>
-      {isOpen && (
-        <div className="dropdown-box">
-          <div className="search-bar px-[14px] py-[15px]">
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder={SearchPlaceholder}
-            />
-          </div>
-          <ul>
-            {filteredItems.map((item) => (
-              <li key={item}>
-                <input
-                  type="checkbox"
-				  id={`checkbox-${item}`}
-                  checked={selectedItems.includes(item)}
-                  onChange={() => handleItemClick(item)}
-                />
-                <label htmlFor={`checkbox-${item}`} className="select-none">{item}</label>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+		<div className="select-item-with-search relative" ref={dropdownRef}>
+			{labeltextItem && labelText && (
+				<label className="label-text text-[16px] text-left text-neu-white font-tertery font-bold leading-normal mb-[5px] block">
+					{labelText}
+				</label>
+			)}
+			<div className="input-item-select-filed-box relative">
+				<input
+					className="input-selected-data"
+					type="text"
+					value={value}
+					onClick={toggleDropdown}
+					placeholder={placeholder}
+					readOnly
+				/>
+				<span className="w-[6px] h-[19px] absolute top-0 bottom-0 right-3 mx-auto">
+					<Image
+						src="/images/icon.svg"
+						alt="arrow icons"
+						width={6}
+						height={19}
+					/>
+				</span>
+				{error ?? <p className="text-red-700">{error}</p>}
+			</div>
+			{isOpen && (
+				<div className="dropdown-box">
+					<div className="search-bar px-[14px] py-[15px]">
+						<input
+							type="text"
+							value={searchTerm}
+							onChange={e => setSearchTerm(e.target.value)}
+							placeholder={SearchPlaceholder}
+						/>
+					</div>
+					<ul>
+						{filteredItems.map(item => (
+							<li key={item}>
+								<label htmlFor={`checkbox-${item}`} className="select-none">
+									<input
+										type="checkbox"
+										id={`checkbox-${item}`}
+										checked={selectedItems.includes(item)}
+										onChange={() => handleItemClick(item)}
+									/>
+									{item}
+								</label>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default SelectItemWithSearch;
