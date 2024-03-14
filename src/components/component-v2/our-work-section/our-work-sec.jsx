858d9c0b2813abaf60efe528e92a1083415beb97
Omolara5861 <debbiegterra@gmail.com>
2024-03-14 06:13:38 +0100

    refactor: :recycle: Refactor our work section

--- src/components/component-v2/our-work-section/our-work-sec.jsx
+++ src/components/component-v2/our-work-section/our-work-sec.jsx
@@ -22,171 +22,174 @@ const OurWorkSec = ({ WorkSecClass, OurWorkSecTitle }) => {
 	const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
 	return (
-		<section
-			className={`"our-work-section pb-[70px] md:pb-20" ${WorkSecClass}`}>
-			<div className="custom-container max-w-[1220px]">
-				<div className="comon-title mb-6">
-					<h2>{OurWorkSecTitle || "Our Work"}</h2>
-					<span></span>
-				</div>
-				<div className="carousel work-slider-main-box pb-6 grid grid-cols-12 gap-[37px] sm:block">
-					<div className="work-slide-main-view col-span-7">
-						<Swiper
-							spaceBetween={10}
-							effect={"fade"}
-							loop={true}
-							autoplay={{
-								delay: 6000,
-								disableOnInteraction: false,
-							}}
-							navigation={false}
-							thumbs={{
-								swiper:
-									thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
-							}}
-							modules={[EffectFade, FreeMode, Navigation, Thumbs, Autoplay]}
-							breakpoints={{
-								640: {
-									slidesPerView: 1,
-									spaceBetween: 20,
-								},
-								768: {
-									slidesPerView: 1,
-									spaceBetween: 40,
-								},
-								1024: {
-									slidesPerView: 1,
-									spaceBetween: 50,
-								},
-							}}
-							fade={true} // Add fade effect
-							className="thumbs w-full rounded-lg">
-							{/* Slides for the main Swiper */}
-							<SwiperSlide>
-								<div className="work-slide-items work-slide-item-one">
-									<div className="client-project-img">
-										<Image
-											src="/images/our-works-img-item-1.jpg"
-											width={657}
-											height={451}
-											className="h-[451px] md:h-auto"
-											alt="our work carousel image"
-										/>
-									</div>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide>
-								<div className="work-slide-items work-slide-item-two">
-									<div className="client-project-img">
-										<Image
-											src="/images/our-works-img-item-2.jpg" // Replace with the correct image path
-											width={657}
-											height={451}
-											className="h-[451px] md:h-auto"
-											alt="our work carousel image"
-										/>
-									</div>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide>
-								<div className="work-slide-items work-slide-item-three">
-									<div className="client-project-img">
-										<Image
-											src="/images/our-works-img-item-3.jpg" // Replace with the correct image path
-											width={657}
-											height={451}
-											className="h-[451px] md:h-auto"
-											alt="our work carousel image"
-										/>
-									</div>
-								</div>
-							</SwiperSlide>
-						</Swiper>
-					</div>
+    <section
+      className={`"our-work-section pb-[70px] md:pb-20" ${WorkSecClass}`}
+    >
+      <div className="custom-container max-w-[1220px]">
+        <div className="comon-title mb-6">
+          <h2>{OurWorkSecTitle || "Our Work"}</h2>
+          <span></span>
+        </div>
+        <div className="carousel work-slider-main-box pb-6 grid grid-cols-12 gap-[37px] sm:block">
+          <div className="work-slide-main-view col-span-7">
+            <Swiper
+              spaceBetween={10}
+              effect={"fade"}
+              loop={true}
+              autoplay={{
+                delay: 6000,
+                disableOnInteraction: false,
+              }}
+              navigation={false}
+              thumbs={{
+                swiper:
+                  thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+              }}
+              modules={[EffectFade, FreeMode, Navigation, Thumbs, Autoplay]}
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 1,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 1,
+                  spaceBetween: 50,
+                },
+              }}
+              fade={true} // Add fade effect
+              className="thumbs w-full rounded-lg"
+            >
+              {/* Slides for the main Swiper */}
+              <SwiperSlide>
+                <div className="work-slide-items work-slide-item-one">
+                  <div className="client-project-img">
+                    <Image
+                      src="/images/our-works-img-item-1.jpg"
+                      width={657}
+                      height={451}
+                      className="h-[451px] md:h-auto"
+                      alt="our work carousel image"
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="work-slide-items work-slide-item-two">
+                  <div className="client-project-img">
+                    <Image
+                      src="/images/our-works-img-item-2.jpg" // Replace with the correct image path
+                      width={657}
+                      height={451}
+                      className="h-[451px] md:h-auto"
+                      alt="our work carousel image"
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="work-slide-items work-slide-item-three">
+                  <div className="client-project-img">
+                    <Image
+                      src="/images/our-works-img-item-3.jpg" // Replace with the correct image path
+                      width={657}
+                      height={451}
+                      className="h-[451px] md:h-auto"
+                      alt="our work carousel image"
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
 
-					{/* Pagination Box */}
-					<div className="clients pagination-box col-span-5">
-						<Swiper
-							onSwiper={setThumbsSwiper}
-							loop={false}
-							spaceBetween={12}
-							slidesPerView={3}
-							freeMode={true}
-							autoplay={{
-								delay: 6000,
-								disableOnInteraction: false,
-							}}
-							watchSlidesProgress={true}
-							modules={[FreeMode, Navigation, Thumbs, Autoplay]}
-							breakpoints={{
-								640: {
-									slidesPerView: 3,
-									spaceBetween: 0,
-								},
-								738: {
-									slidesPerView: 3,
-									spaceBetween: 0,
-								},
-								1024: {
-									slidesPerView: 3,
-									spaceBetween: 0,
-								},
-							}}
-							className="w-full rounded-lg">
-							{/* Slides for the pagination Swiper */}
-							<SwiperSlide>
-								<div className="client-naeo project-info paginetion-item relative pagination-active">
-									<p className="name mb-[6px]">Hult IB School</p>
-									<div className="active-slide-cont-box">
-										<p className="description mb-4">
-											Crafting a web design and Paid Ads solution to help
-											deliver on Hult Business School’s ongoing desire for
-											increased student enrolment.
-										</p>
-										<Link href="/case-studies/hult-ib-school">
-											Full Case Study
-										</Link>
-										<div className="progress-loader"></div>
-									</div>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide>
-								<div className="project-info paginetion-item relative">
-									<p className="name  mb-[6px]">AI & Humanity</p>
-									<div className="active-slide-cont-box">
-										<p className="description mb-4">
-											Design intention: to symbolise the harmonious coexistence
-											of AI and humanity, ensuring the campaign's messaging
-											would be easily digestible for a contemporary,
-											fast-evolving audience.
-										</p>
-										<Link href="/case-studies/ai-and-humanity">
-											Full Case Study
-										</Link>
-										<div className="progress-loader"></div>
-									</div>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide>
-								<div className="project-info paginetion-item relative">
-									<p className="name  mb-[6px]">Zenbase</p>
-									<div className="active-slide-cont-box">
-										<p className="description mb-4">
-											Elevating Zenbase's financial health solutions, our UX/UI
-											design enhances user empowerment and brand resonance,
-											cultivating an inclusive digital experience.
-										</p>
-										<Link href="/case-studies/zenbase">Full Case Study</Link>
-										<div className="progress-loader"></div>
-									</div>
-								</div>
-							</SwiperSlide>
-						</Swiper>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+          {/* Pagination Box */}
+          <div className="clients pagination-box col-span-5">
+            <Swiper
+              onSwiper={setThumbsSwiper}
+              loop={false}
+              spaceBetween={12}
+              slidesPerView={3}
+              freeMode={true}
+              autoplay={{
+                delay: 6000,
+                disableOnInteraction: false,
+              }}
+              watchSlidesProgress={true}
+              modules={[FreeMode, Navigation, Thumbs, Autoplay]}
+              breakpoints={{
+                640: {
+                  slidesPerView: 3,
+                  spaceBetween: 0,
+                },
+                738: {
+                  slidesPerView: 3,
+                  spaceBetween: 0,
+                },
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 0,
+                },
+              }}
+              className="w-full rounded-lg"
+            >
+              {/* Slides for the pagination Swiper */}
+              <SwiperSlide>
+                <div className="client-naeo project-info paginetion-item relative pagination-active">
+                  <p className="name mb-[6px]">Hult IB School</p>
+                  <div className="active-slide-cont-box">
+                    <p className="description mb-4">
+                      Transforming Naeo's digital user experience through a
+                      user-focused web design and holistic brand identity,
+                      enhancing engagement, credibility, and sustainability
+                    </p>
+                    <Link href="/case-studies/hult-ib-school">
+                      Full Case Study
+                    </Link>
+                    <div className="progress-loader"></div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="project-info paginetion-item relative">
+                  <p className="name  mb-[6px]">Knights Academy</p>
+                  <div className="active-slide-cont-box">
+                    <p className="description mb-4">
+                      Design intention: to symbolise the harmonious coexistence
+                      of AI and humanity, ensuring the campaign's messaging
+                      would be easily digestible for a contemporary,
+                      fast-evolving audience.
+                    </p>
+                    <Link href="/case-studies/ai-and-humanity">
+                      Full Case Study
+                    </Link>
+                    <div className="progress-loader"></div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="project-info paginetion-item relative">
+                  <p className="name  mb-[6px]">Zenbase</p>
+                  <div className="active-slide-cont-box">
+                    <p className="description mb-4">
+                      Elevating Zenbase's financial health solutions, our UX/UI
+                      design enhances user empowerment and brand resonance,
+                      cultivating an inclusive digital experience.
+                    </p>
+                    <Link href="/case-studies/zenbase">Full Case Study</Link>
+                    <div className="progress-loader"></div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default OurWorkSec;
