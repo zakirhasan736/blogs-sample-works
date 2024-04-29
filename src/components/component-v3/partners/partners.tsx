e0516f2b16931bf8d31327aba8d4c5e09dd60ee4
Omolara5861 <debbiegterra@gmail.com>
2024-04-29 18:19:26 +0100

    feat: :sparkles: Add partners

---
+++ src/components/component-v3/partners/partners.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React, { useState } from "react";
+import { Image, Link } from "@packages/packages";
+
+
+const TestimonialSec = () => {
+
+	return (
+    <section className="testimonials bg-[#fff] text-[#181725] pt-[101px] pb-[48px]">
+      <div className="custom-container">
+          <div className="flex gap-10">
+            <h2 className="text-[#181725]">Testimonials</h2>
+            <p>
+              Our clients have amazing things to say about Particular. Read what{" "}
+              <br />
+              they have to say.
+            </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TestimonialSec;
