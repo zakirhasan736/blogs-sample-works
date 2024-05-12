891e169761aa9a1eeb46f99cdbedc6cc044992d6
omolara5861 <debbiegterra@gmail.com>
2024-05-12 17:07:07 +0100

    fix: :pencil2: Fix spelling error

--- src/pageComponents/v3/DrJohnTsagaris/banner/banner.tsx
+++ src/pageComponents/v3/DrJohnTsagaris/banner/banner.tsx
@@ -14,7 +14,7 @@ const Banner: React.FC<HomeBannerProps> = () => {
       <div className="custom-container">
         {/* Content */}
         <div className="absolute bottom-[48px] text-white">
-          <h1>Dr John Tsagaris</h1>
+          <h1>Dr John Tsagiris</h1>
           <h4>A Digital Transformation Project</h4>
         </div>
       </div>
