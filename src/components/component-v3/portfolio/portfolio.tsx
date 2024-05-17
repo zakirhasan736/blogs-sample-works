0bdc96985e437f8ccccddb461308222ba602fa62
omolara5861 <debbiegterra@gmail.com>
2024-05-17 15:36:01 +0100

    feat: :sparkles: Make portfolio link

--- src/components/component-v3/portfolio/portfolio.tsx
+++ src/components/component-v3/portfolio/portfolio.tsx
@@ -61,37 +61,39 @@ const PortfolioSec = () => {
               >
                 {PortfolioSlideData.map((portfolio, index) => (
                   <SwiperSlide key={index}>
-                    <div className="portfolio-card-item max-w-[344px] w-full relative">
-                      <div className="portfolio-card-img rounded-[4px] max-w-[344px] w-full max-h-[424px] h-full">
-                        <Image
-                          src={`/images/portfolio/${portfolio.cardImg}`}
-                          alt="portfolio thumbnail image"
-                          className="card-image rounded-[4px] w-full h-full"
-                          width={344}
-                          height={424}
-                        />
-                      </div>
-                      <div className="portfolio-card-cont absolute top-0 left-0 w-full h-full flex flex-col">
-                        <div className="card-logo py-6 px-[6px] bg-white max-w-[75px] w-full h-[66px] rounded-tl-[4px]">
-                          {portfolio.cardImgLogo ? (
-                            <Image
-                              src={`/images/portfolio/${portfolio.cardImgLogo}`}
-                              alt="portfolio project brand logo"
-                              width={62}
-                              height={17}
-                            />
-                          ) : null}
+                    <Link href={`/${portfolio.link}`} className="!text-white">
+                      <div className="portfolio-card-item max-w-[344px] w-full relative">
+                        <div className="portfolio-card-img rounded-[4px] max-w-[344px] w-full max-h-[424px] h-full">
+                          <Image
+                            src={`/images/portfolio/${portfolio.cardImg}`}
+                            alt="portfolio thumbnail image"
+                            className="card-image rounded-[4px] w-full h-full"
+                            width={344}
+                            height={424}
+                          />
                         </div>
-                        <div className="portfolio-info mt-auto w-full px-4 py-[24px]">
-                          <h4 className="card-subtitle uppercase font-secondary text-[14px] sm:text-[11px] font-normal tracking-[2.1px] mb-[9px] leading-[16px]">
-                            {portfolio.cardSubTitle}
-                          </h4>
-                          <h5 className="card-title sm:text-[12px] text-[24px] text-left text-white font-primary font-bold tracking-[2.1px] uppercase leading-[120%]">
-                            {portfolio.cardTitle}
-                          </h5>
+                        <div className="portfolio-card-cont absolute top-0 left-0 w-full h-full flex flex-col">
+                          <div className="card-logo py-6 px-[6px] bg-white max-w-[75px] w-full h-[66px] rounded-tl-[4px]">
+                            {portfolio.cardImgLogo ? (
+                              <Image
+                                src={`/images/portfolio/${portfolio.cardImgLogo}`}
+                                alt="portfolio project brand logo"
+                                width={62}
+                                height={17}
+                              />
+                            ) : null}
+                          </div>
+                          <div className="portfolio-info mt-auto w-full px-4 py-[24px]">
+                            <h4 className="card-subtitle uppercase font-secondary text-[14px] sm:text-[11px] font-normal tracking-[2.1px] mb-[9px] leading-[16px]">
+                              {portfolio.cardSubTitle}
+                            </h4>
+                            <h5 className="card-title sm:text-[12px] text-[24px] text-left text-white font-primary font-bold tracking-[2.1px] uppercase leading-[120%]">
+                              {portfolio.cardTitle}
+                            </h5>
+                          </div>
                         </div>
                       </div>
-                    </div>
+                    </Link>
                   </SwiperSlide>
                 ))}
               </Swiper>
