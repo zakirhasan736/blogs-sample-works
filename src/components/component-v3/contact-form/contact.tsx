0674d82affe62a5b20f8522d450f51bf9da331d6
Omolara5861 <debbiegterra@gmail.com>
2024-05-23 06:04:48 +0100

    feat: :sparkles: Add submit case to contact

--- src/components/component-v3/contact-form/contact.tsx
+++ src/components/component-v3/contact-form/contact.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 interface ContactFormProps {
@@ -142,39 +143,15 @@ const ContactForm: React.FC<ContactFormProps> = ({ className }) => {
   return (
     <>
       {formSubmitted && (
-        <div className="final-steps">
-          <div className="confirmationcard-item flex items-start gap-[10px]">
-            <div className="confirmation-check-mark">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-              >
-                <path
-                  d="M22 11.0799V11.9999C21.9988 14.1563 21.3005 16.2545 20.0093 17.9817C18.7182 19.7088 16.9033 20.9723 14.8354 21.5838C12.7674 22.1952 10.5573 22.1218 8.53447 21.3744C6.51168 20.6271 4.78465 19.246 3.61096 17.4369C2.43727 15.6279 1.87979 13.4879 2.02168 11.3362C2.16356 9.18443 2.99721 7.13619 4.39828 5.49694C5.79935 3.85768 7.69279 2.71525 9.79619 2.24001C11.8996 1.76477 14.1003 1.9822 16.07 2.85986"
-                  stroke="#143215"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M22 4L12 14.01L9 11.01"
-                  stroke="#143215"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </div>
-            <div className="confirmation-msg-main">
-              <p className="conf-desc mb-[6px] text-[#143215] text-[16px] text-left font-normal font-tertery leading-[28px]">
-                Thank you for your submission. We will be in touch soon.
+            <div className="confirmation-msn mt-[88px]">
+              <Image src="/images/con-check.svg" alt="Check" width={16} height={16} />
+              <p className=" mb-[6px] mt-3  text-[18px] font-normal leading-[28px]">
+                Thank you for your enquiry,
+                <br />
+                <br />
+                We will be in touch soon.
               </p>
             </div>
-          </div>
-        </div>
       )}
 
       {!formSubmitted && (
