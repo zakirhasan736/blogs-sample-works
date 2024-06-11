a84ae7fd694965ea448af150a825a6946f1f3faa
Omolara5861 <debbiegterra@gmail.com>
2024-06-11 17:53:28 +0100

    feat: :sparkles: Add animation to homepage

--- src/components/component-v3/portfolio/portfolio.tsx
+++ src/components/component-v3/portfolio/portfolio.tsx
@@ -1,42 +1,65 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import { Image, Link } from "@packages/packages";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/free-mode";
 import PortfolioSlideData from "@data/portfolioSliderData/PortfolioSlideData.json";
+
 const PortfolioSec = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, {
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   return (
-    <section className="portfolio  pt-[64px]  sm:pt-8 pb-[106px] sm:pb-[53px] bg-[#E1E4E8]">
+    <section className="portfolio pt-[64px] sm:pt-8 pb-[106px] sm:pb-[53px] bg-[#E1E4E8]">
       <div className="custom-container">
-        <div className=" mb-[47px] sm:mb-[35px] sm:flex-col sm:items-start sm:gap-5">
+        <div className="mb-[47px] sm:mb-[35px] sm:flex-col sm:items-start sm:gap-5">
           <div className="section-title-box flex sm:!flex-row justify-between mb-2">
-            <h3 className="text-[#181725]">Particular Portfolio</h3>
+            <motion.h3
+              className="text-[#181725]"
+              ref={ref}
+              initial={{ opacity: 0, y: -100 }}
+              animate={isInView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 0.8 }}
+            >
+              Particular Portfolio
+            </motion.h3>
           </div>
-          <p className="text-[#000]">
+          <motion.p
+            className="text-[#000]"
+            initial={{ opacity: 0, y: -100 }}
+            animate={isInView ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
             We pioneer what's next, combining creativity, tech innovation and
             data.
-          </p>
+          </motion.p>
         </div>
       </div>
-      <div className="portfolio-card-item-box">
+      <div
+        className="portfolio-card-item-box"
+        // initial={{ opacity: 0, y: -100 }}
+        // animate={isInView ? { opacity: 1, y: 0 } : {}}
+        // transition={{ duration: 0.8, delay: 0.6 }}
+      >
         <div className="custom-container-full max-w-[1440px] w-full mx-auto pl-[140px] laptop-m:pl-[30px] md:pl-[20px] sm:pl-4">
-          <div className="portfolio-card-slide-container pb-4  !overflow-hidden">
+          <div className="portfolio-card-slide-container pb-4 !overflow-hidden">
             <div className="portfolio-card-item-wrapper max-w-[1056px] md:max-w-[500px] sm:max-w-[280px] w-full ml-0">
               <Swiper
                 spaceBetween={10}
-                // loop={true}
-                // centeredSlides={true}
                 freeMode={true}
-                autoplay={{
-                  delay: 6000,
-                  disableOnInteraction: false,
-                }}
-                navigation={false}
+                // autoplay={{
+                //   delay: 6000,
+                //   disableOnInteraction: false,
+                // }}
+                navigation={true}
                 modules={[FreeMode, Autoplay]}
                 breakpoints={{
                   640: {
@@ -61,7 +84,12 @@ const PortfolioSec = () => {
                 {PortfolioSlideData.map((portfolio, index) => (
                   <SwiperSlide key={index}>
                     <Link href={`/${portfolio.link}`} className="!text-white">
-                      <div className="portfolio-card-item max-w-[344px] w-full relative">
+                      <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={isInView ? { opacity: 1, y: 0 } : {}}
+                        transition={{ duration: 0.8, delay: index * 0.5 }}
+                        className="portfolio-card-item max-w-[344px] w-full relative"
+                      >
                         <div className="portfolio-card-img rounded-[4px] max-w-[344px] w-full max-h-[424px] h-full">
                           <Image
                             src={`/images/portfolio/${portfolio.cardImg}`}
@@ -91,7 +119,7 @@ const PortfolioSec = () => {
                             </h5>
                           </div>
                         </div>
-                      </div>
+                      </motion.div>
                     </Link>
                   </SwiperSlide>
                 ))}
