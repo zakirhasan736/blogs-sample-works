3e44be21443d2a2212c0e5ad2cbf77a280618784
Omolara5861 <debbiegterra@gmail.com>
2024-06-28 11:03:10 +0100

    feat: :sparkles: Add portfolio

--- src/components/component-v3/contact/contact.tsx
+++ src/components/component-v3/contact/contact.tsx
@@ -13,19 +13,18 @@ const ContactSec = () => {
   });
 
   return (
-    <motion.section
-      className="contact-sec bg-white"
-      ref={sectionRef}
-      initial={{ opacity: 0 }}
-      animate={isInView ? { opacity: 1} : {}}
-      transition={{ duration: 0.5 }}
-    >
+    <section className="contact-sec bg-white">
       <div className="contact-sec-wrappr h-full max-w-[1920px] w-full mx-auto grid gap-[90px] md:gap-[45px] sm:gap-[30px] grid-cols-12 sm:grid-cols-6">
         <motion.div
           className="contact-modal-box w-full h-full col-span-6 md:col-span-5 sm:col-span-6"
-          initial={{ opacity: 0}}
-          animate={isInView ? { opacity: 1} : {}}
-          transition={{ duration: 0.5, delay: 0.2 }}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={{
+            visible: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          transition={{ duration: 1, ease: 'easeIn', delay: 1 }}
         >
           <Image
             src="/images/contact.png"
@@ -37,16 +36,21 @@ const ContactSec = () => {
         </motion.div>
         <motion.div
           className="contact-form col-span-6 md:col-span-7 sm:col-span-6 pt-[74px] sm:pt-0 pb-[50px] md:pt-[40px] sm:py-8 pr-[176px] lg:pr-[25px] md:pr-[20px] sm:px-4"
-          initial={{ opacity: 0 }}
-          animate={isInView ? { opacity: 1 } : {}}
-          transition={{ duration: 0.5, delay: 0.6 }}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={{
+            visible: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          transition={{ duration: 1, eas: 'easeIn', delay: 1 }}
         >
           <h3 className="mb-2">Ready to get started?</h3>
           <p>Fast track your growth with Particular</p>
           <ContactForm className="btn-dark" />
         </motion.div>
       </div>
-    </motion.section>
+    </section>
   );
 };
 
