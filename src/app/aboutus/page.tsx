8b0d7551848c448a93d556b2cbeb90060395753e
Omolara5861 <debbiegterra@gmail.com>
2024-07-06 15:00:00 +0100

    Change meta information

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -3,9 +3,9 @@ import AboutUsPage from "@/pageComponents/aboutus/aboutus";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "About Us - Particular Agency London",
-	description:
-		"Agency's evolution spans over 15 years, a journey that began when a group of forward-thinking kids first identified the pervasive problem of underwhelming quality in the digital world. From those early days to now, our team has grown into a dedicated group of seasoned professionals, all committed to addressing this challenge head-on.",
+  title: "About Us - Particular Agency",
+  description:
+    "Non-Customer and Customer Experience are undervalued in the UK and beyond. Thoughtfulness is the first step to solving that problem. Think Particular",
 };
 
 export default function page() {
